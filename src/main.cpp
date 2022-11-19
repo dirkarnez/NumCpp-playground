cc7613b3d5ab76a7795b20de049480479113ab10
dirkarnez <smalldirkalex@gmail.com>
2022-11-19 17:25:07 +0800

    - fix .vscode\c_cpp_properties.json

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
-#include "NumCpp.hpp"
 #include <iostream>
+#include "NumCpp.hpp"
 
 int main()
 {
 	// [[1, 3, 5, 7, 10, ]]
-	auto a = nc::linspace(1, 10, 5);
+	const nc::NdArray<int> a = nc::linspace(1, 10, 5);
 	std::cout << a << std::endl;
-    return 0;
+	std::cout << a.at(0) << std::endl;
+	std::cin.get();
+	return 0;
 }
